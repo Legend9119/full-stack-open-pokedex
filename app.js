f9d0abb0b3bfcea34d68e2fbeb66534d7fd23557
Nicholas Hendry <nicholaskhendry@gmail.com>
2024-08-19 21:31:33 +1000

    Add a health check for the Fly IO deployment. Deliberately throw an exception for the health check endpoint, so that Fly IO stops the rolling out to production.

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
+app.get('/version', (req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
 })
 
-app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
 })
